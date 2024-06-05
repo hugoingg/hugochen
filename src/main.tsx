1bedb7b1455ac8984ceb9f14590a34232e1b078d
Hugo Chen <l.hugochen@gmail.com>
2024-06-05 16:29:41 +0800

    try: project page as main

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/hugochen/",
-        element: <HomePage />,
+        element: <ProjectPage />,
       },
       {
         path: "/hugochen/projects",

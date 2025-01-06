9a7ee6e2e53636491af67347e364ac8a4b2c6bdd
Hugo Chen <l.hugochen@gmail.com>
2025-01-06 12:26:58 +0800

    Added: video in soil. Soil page is finalized

--- src/projectPages/soil.tsx
+++ src/projectPages/soil.tsx
@@ -1,3 +1,5 @@
+import video1 from "../media/soil-pics/soil1.mp4";
+
 function Soil() {
   return (
     <div className="projects-Pages">
@@ -12,14 +14,25 @@ function Soil() {
           AI model to predict the amount of Carbon emission produced due rice
           cultivation in Thailand. We started by collecting data from the
           internet that can be our features and targets to train the model.
-          Afterwards, we clean the data of unnecessary columns, and checked the
-          linearity of each features against the target. We initially used 4
+          Afterwards, we cleaned the data of unnecessary columns, and checked
+          the linearity of each features against the target. We initially used 4
           features: rice yield in a year, Nitrogen, Phosphorus and Potassium
           fertilizers used, and 1 target: the amount of Carbon emission due rice
-          cultivation.
+          cultivation. After mutliple model testings, we found that the optimal
+          model does not need to use Phosphorus fertilizer as its feature, and
+          that it is more fitting touse the log of base 10 values of the other
+          two fertilizers.
         </p>
         <div className="projects-Pages-content">
-          <p></p>
+          <p>
+            We proceeded to optimize the performance of our model using
+            hyperparameter tuning and data pre-processing with Scikit-learn.
+            After the model is finished, I built a simple web application to
+            implement the model using Flask, and deployed it on Vocareum.
+          </p>
+        </div>
+        <div className="project-Pages-Photos-Container">
+          <video src={video1} controls></video>
         </div>
       </div>
     </div>
